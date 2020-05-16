deb4bfb6dab6768cf6903c133de59e6a14b55ff4
Amie333 <amiewood01@gmail.com>
2020-05-16 18:38:39 +0100

    new

--- Succulentwebsite/Controllers/HomeController.cs
+++ Succulentwebsite/Controllers/HomeController.cs
@@ -64,8 +64,139 @@ namespace Succulentwebsite.Controllers
                     result.Title = "";
 
                     break;
-                }
 
+                case "rabbit":
+
+                    result.ImageUrl = "/content/images/animals/rabbit.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "fish":
+
+                    result.ImageUrl = "/content/images/animals/fish.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "fish2":
+
+                    result.ImageUrl = "/content/images/animals/fish2.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "tortiose":
+
+                    result.ImageUrl = "/content/images/animals/tortiose.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "whales":
+
+                    result.ImageUrl = "/content/images/animals/whales.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "lizards":
+
+                    result.ImageUrl = "/content/images/animals/lizards.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "insects":
+
+                    result.ImageUrl = "/content/images/animals/insects.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "horses":
+
+                    result.ImageUrl = "/content/images/animals/horses.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "pigs":
+
+                    result.ImageUrl = "/content/images/animals/pigs.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "lions":
+
+                    result.ImageUrl = "/content/images/animals/lions.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "cacti":
+
+                    result.ImageUrl = "/content/images/plants/cacti.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "palm":
+
+                    result.ImageUrl = "/content/images/plants/palm.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "lillies":
+
+                    result.ImageUrl = "/content/images/plants/lillies.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "rose":
+
+                    result.ImageUrl = "/content/images/plants/rose.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "succulent":
+
+                    result.ImageUrl = "/content/images/plants/succulent.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+
+                case "wildherbs":
+
+                    result.ImageUrl = "/content/images/plants/wildherbs.png";
+                    result.Description = "";
+                    result.Title = "";
+
+                    break;
+            }
+
+            
+            
+       
                 return View(result);
             }
         }
